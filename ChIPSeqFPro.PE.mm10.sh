e0c21f106c2cd81c556f472b0ea1e3ea32551ce5
Milos Pjanic <pjanic.milos@gmail.com>
2017-03-18 16:59:20 -0700

    Update ChIPSeqFPro.PE.mm10.sh

--- ChIPSeqFPro.PE.mm10.sh
+++ ChIPSeqFPro.PE.mm10.sh
@@ -53,7 +53,7 @@ cat >> commands.2.${files[i]}.${files[i+1]}.tmp <<EOL
     ./bam2bigwig.mm10.sh ${files[i]}.${files[i+1]}.BWA/${files[i]}.${files[i+1]}.bam
     
     #running MACS2 on bam
-    macs14 -t ${files[i]}.${files[i+1]}.BWA/${files[i]}.${files[i+1]}.bam -g mm -n ${files[i]}.${files[i+1]}.MACS2
+    macs2 -t ${files[i]}.${files[i+1]}.BWA/${files[i]}.${files[i+1]}.bam -g mm -n ${files[i]}.${files[i+1]}.MACS2
 
 EOL
   done
